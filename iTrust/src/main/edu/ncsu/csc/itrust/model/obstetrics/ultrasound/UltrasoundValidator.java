407e079bafa693322bcb1395c3d5dca06eeee9a2
James Gonzales <jcgonzal@ncsu.edu>
2017-03-26 16:46:00 -0400

    Validator tweaks and validator tests

--- iTrust/src/main/edu/ncsu/csc/itrust/model/obstetrics/ultrasound/UltrasoundValidator.java
+++ iTrust/src/main/edu/ncsu/csc/itrust/model/obstetrics/ultrasound/UltrasoundValidator.java
@@ -14,31 +14,53 @@ public class UltrasoundValidator extends POJOValidator<Ultrasound> {
 	public void validate(Ultrasound obj) throws FormValidationException {
 		ErrorList errorList = new ErrorList();
 		
-		if (obj.getCrl() <= 0)
+		if (obj.getCrl() == null)
+			errorList.addIfNotNull("CRL is required");
+		
+		else if (obj.getCrl() <= 0)
 			errorList.addIfNotNull("CRL must be greater than zero");
 		
-		if (obj.getBpd() <= 0)
+		if (obj.getBpd() == null)
+			errorList.addIfNotNull("BPD is required");
+		
+		else if (obj.getBpd() <= 0)
 			errorList.addIfNotNull("BPD must be greater than zero");
 		
-		if (obj.getHc() <= 0)
+		if (obj.getHc() == null)
+			errorList.addIfNotNull("HC is required");
+		
+		else if (obj.getHc() <= 0)
 			errorList.addIfNotNull("HC must be greater than zero");
 		
-		if (obj.getFl() <= 0)
+		if (obj.getFl() == null)
+			errorList.addIfNotNull("FL is required");
+		
+		else if (obj.getFl() <= 0)
 			errorList.addIfNotNull("FL must be greater than zero");
 		
-		if (obj.getOfd() <= 0)
+		if (obj.getOfd() == null)
+			errorList.addIfNotNull("OFD is required");
+		
+		else if (obj.getOfd() <= 0)
 			errorList.addIfNotNull("OFD must be greater than zero");
 		
-		if (obj.getAc() <= 0)
+		if (obj.getAc() == null)
+			errorList.addIfNotNull("AC is required");
+		
+		else if (obj.getAc() <= 0)
 			errorList.addIfNotNull("AC must be greater than zero");
 		
-		if (obj.getHl() <= 0)
+		if (obj.getHl() == null)
+			errorList.addIfNotNull("HL is required");
+		
+		else if (obj.getHl() <= 0)
 			errorList.addIfNotNull("HL must be greater than zero");
 		
-		if (obj.getEfw() <= 0)
-			errorList.addIfNotNull("EFW must be greater than zero");
+		if (obj.getEfw() == null)
+			errorList.addIfNotNull("EFW is required");
 		
-		//TODO: There might need to be more validation here
+		else if (obj.getEfw() <= 0)
+			errorList.addIfNotNull("EFW must be greater than zero");
 		
 		if ( errorList.hasErrors() )
 			throw new FormValidationException(errorList);		
---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetrics/visit/ObstetricsVisitValidatorTest.java
@@ -0,0 +1,77 @@
+package edu.ncsu.csc.itrust.unit.model.obstetrics.visit;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import javax.sql.rowset.serial.SerialBlob;
+import javax.sql.rowset.serial.SerialException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ncsu.csc.itrust.exception.FormValidationException;
+import edu.ncsu.csc.itrust.model.obstetrics.visit.ObstetricsVisit;
+import edu.ncsu.csc.itrust.model.obstetrics.visit.ObstetricsVisitValidator;
+
+public class ObstetricsVisitValidatorTest {
+
+	ObstetricsVisitValidator validator;
+	
+	@Before
+	public void setUp() throws Exception {
+			validator = new ObstetricsVisitValidator();
+	}
+
+	@Test
+	public void testValidateUltrasound() {
+		
+		byte[] b1 = new byte[100];
+		Arrays.fill(b1, (byte)0);
+		ObstetricsVisit goodObj;
+		try {
+			goodObj = new ObstetricsVisit(new Long(2), new Integer(13), new Integer(54), new Integer(3), new Boolean(true), new SerialBlob(b1));
+			validator.validate(goodObj);
+		} catch (SerialException e1) {
+			Assert.fail(e1.getMessage());
+		} catch (SQLException e1) {
+			Assert.fail(e1.getMessage());
+		} catch (FormValidationException e) {
+			Assert.fail(e.getMessage());
+		}
+		
+		
+		try {
+			ObstetricsVisit badObj1 = new ObstetricsVisit(new Long(2), null, null, null, null, null);
+			validator.validate(badObj1);
+			Assert.fail("Validation passed for an invalid Obstetrics Visit");
+		} catch (FormValidationException e) {
+			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant is required, Multiplicity is required, Fetal heart rate is required, Low lying placenta observation is required, Image of ultrasound is required]"));
+		}
+		
+		try {
+			ObstetricsVisit badObj2 = new ObstetricsVisit(new Long(2), new Integer(-1), new Integer(0), new Integer(0), new Boolean(true), new SerialBlob(b1));
+			validator.validate(badObj2);
+			Assert.fail("Validation passed for an invalid Obstetrics Visit");
+		} catch (FormValidationException e) {
+			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
+		} catch (SerialException e1) {
+			Assert.fail(e1.getMessage());
+		} catch (SQLException e1) {
+			Assert.fail(e1.getMessage());
+		}
+		
+		try {
+			ObstetricsVisit badObj3 = new ObstetricsVisit(new Long(2), new Integer(43), new Integer(-1), new Integer(-1), new Boolean(false), new SerialBlob(b1));
+			validator.validate(badObj3);
+			Assert.fail("Validation passed for an invalid Obstetrics Visit");
+		} catch (FormValidationException e) {
+			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
+		} catch (SerialException e1) {
+			Assert.fail(e1.getMessage());
+		} catch (SQLException e1) {
+			Assert.fail(e1.getMessage());
+		}
+	}
+
+}
