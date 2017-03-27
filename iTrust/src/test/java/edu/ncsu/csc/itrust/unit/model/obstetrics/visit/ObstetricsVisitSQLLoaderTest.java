5de533d4400b93403e13dd78dccc3880ab761db6
James Gonzales <jcgonzal@ncsu.edu>
2017-03-27 11:07:21 -0400

    Start ObstetricsVisitSQLLoaderTest

---
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetrics/visit/ObstetricsVisitSQLLoaderTest.java
@@ -0,0 +1,38 @@
+package edu.ncsu.csc.itrust.unit.model.obstetrics.visit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ncsu.csc.itrust.model.obstetrics.visit.ObstetricsVisitSQLLoader;
+
+/**
+ * Tests for obstetrics visit sql loader
+ * @author jcgonzal
+ */
+public class ObstetricsVisitSQLLoaderTest {
+
+	ObstetricsVisitSQLLoader loader;
+	
+	@Before
+	public void setUp() throws Exception {
+		loader = new ObstetricsVisitSQLLoader();
+	}
+
+	@Test
+	public void testLoadList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadSingle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadParameters() {
+		fail("Not yet implemented");
+	}
+
+}
