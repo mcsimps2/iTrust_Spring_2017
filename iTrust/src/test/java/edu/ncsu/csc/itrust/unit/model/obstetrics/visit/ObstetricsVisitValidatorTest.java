0da186fcce4eb64169ff5be755849012390f28c7
amcheshi <amcheshi@ncsu.edu>
2017-03-28 10:40:30 -0400

    Update Unit Tests that were failing due to changes made for new features

--- iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetrics/visit/ObstetricsVisitValidatorTest.java
+++ iTrust/src/test/java/edu/ncsu/csc/itrust/unit/model/obstetrics/visit/ObstetricsVisitValidatorTest.java
@@ -1,10 +1,6 @@
 package edu.ncsu.csc.itrust.unit.model.obstetrics.visit;
 
-import java.sql.SQLException;
 import java.util.Arrays;
-
-import javax.sql.rowset.serial.SerialBlob;
-import javax.sql.rowset.serial.SerialException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -35,19 +31,15 @@ public class ObstetricsVisitValidatorTest {
 		Arrays.fill(b1, (byte)0);
 		ObstetricsVisit goodObj;
 		try {
-			goodObj = new ObstetricsVisit(new Long(2), new Integer(13), new Integer(54), new Integer(3), new Boolean(true), new SerialBlob(b1));
+			goodObj = new ObstetricsVisit(new Long(2), new Integer(13), new Integer(54), new Integer(3), new Boolean(true), null, null);
 			validator.validate(goodObj);
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
 		} catch (FormValidationException e) {
 			Assert.fail(e.getMessage());
 		}
 		
 		
 		try {
-			ObstetricsVisit badObj1 = new ObstetricsVisit(new Long(2), null, null, null, null, null);
+			ObstetricsVisit badObj1 = new ObstetricsVisit(new Long(2), null, null, null, null, null, null);
 			validator.validate(badObj1);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
@@ -55,27 +47,19 @@ public class ObstetricsVisitValidatorTest {
 		}
 		
 		try {
-			ObstetricsVisit badObj2 = new ObstetricsVisit(new Long(2), new Integer(-1), new Integer(0), new Integer(0), new Boolean(true), new SerialBlob(b1));
+			ObstetricsVisit badObj2 = new ObstetricsVisit(new Long(2), new Integer(-1), new Integer(0), new Integer(0), new Boolean(true), null, null);
 			validator.validate(badObj2);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
-			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
+			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 48, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
 		}
 		
 		try {
-			ObstetricsVisit badObj3 = new ObstetricsVisit(new Long(2), new Integer(43), new Integer(-1), new Integer(-1), new Boolean(false), new SerialBlob(b1));
+			ObstetricsVisit badObj3 = new ObstetricsVisit(new Long(2), new Integer(49), new Integer(-1), new Integer(-1), new Boolean(false), null, null);
 			validator.validate(badObj3);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
-			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
+			Assert.assertEquals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 48, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]", e.getMessage());
 		}
 	}
 	
@@ -84,31 +68,23 @@ public class ObstetricsVisitValidatorTest {
 		byte[] b1 = new byte[100];
 		Arrays.fill(b1, (byte)0);
 		try {
-			ObstetricsVisit goodObj = new ObstetricsVisit(new Long(2), new Integer(13), new Integer(54), new Integer(3), new Boolean(true), new SerialBlob(b1));
+			ObstetricsVisit goodObj = new ObstetricsVisit(new Long(2), new Integer(13), new Integer(54), new Integer(3), new Boolean(true), null, null);
 			validator.validateUpdate(goodObj);
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
 		} catch (FormValidationException e) {
 			Assert.fail(e.getMessage());
 		}
 		
 		
 		try {
-			ObstetricsVisit goodObj2 = new ObstetricsVisit(new Long(2), new Integer(13), null, null, null, new SerialBlob(b1));
+			ObstetricsVisit goodObj2 = new ObstetricsVisit(new Long(2), new Integer(13), null, null, null, null, null);
 			validator.validateUpdate(goodObj2);
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
 		} catch (FormValidationException e) {
 			Assert.fail(e.getMessage());
 		}
 		
 		
 		try {
-			ObstetricsVisit badObj1 = new ObstetricsVisit(new Long(2), null, null, null, null, null);
+			ObstetricsVisit badObj1 = new ObstetricsVisit(new Long(2), null, null, null, null, null, null);
 			validator.validate(badObj1);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
@@ -116,27 +92,19 @@ public class ObstetricsVisitValidatorTest {
 		}
 		
 		try {
-			ObstetricsVisit badObj2 = new ObstetricsVisit(new Long(2), new Integer(-1), new Integer(0), new Integer(0), new Boolean(true), new SerialBlob(b1));
+			ObstetricsVisit badObj2 = new ObstetricsVisit(new Long(2), new Integer(-1), new Integer(0), new Integer(0), new Boolean(true), null, null);
 			validator.validate(badObj2);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
-			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
+			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 48, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
 		}
 		
 		try {
-			ObstetricsVisit badObj3 = new ObstetricsVisit(new Long(2), new Integer(43), new Integer(-1), new Integer(-1), new Boolean(false), new SerialBlob(b1));
+			ObstetricsVisit badObj3 = new ObstetricsVisit(new Long(2), new Integer(49), new Integer(-1), new Integer(-1), new Boolean(false), null, null);
 			validator.validate(badObj3);
 			Assert.fail("Validation passed for an invalid Obstetrics Visit");
 		} catch (FormValidationException e) {
-			Assert.assertTrue(e.getMessage().equals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 42, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]"));
-		} catch (SerialException e1) {
-			Assert.fail(e1.getMessage());
-		} catch (SQLException e1) {
-			Assert.fail(e1.getMessage());
+			Assert.assertEquals("This form has not been validated correctly. The following field are not properly filled in: [Weeks pregnant must be between 0 and 48, Multiplicity must be greater than zero, Fetal heart rate must be greater than zero]", e.getMessage());
 		}
 	}
 
