d1567b25514f35c6bdf65ea3f458eaa567f6c55c
Matthew Simpson <mcsimps2@ncsu.edu>
2017-03-29 03:13:03 -0400

    Fixed failing cucumber tests by adding more information to the new office visit in the DB

--- iTrust/sql/data/obstetricsVisit.sql
+++ iTrust/sql/data/obstetricsVisit.sql
@@ -1,2 +1,2 @@
-INSERT INTO officeVisit(patientMID, visitDate, locationID, apptTypeID) VALUES (1, "2017-02-22 00:00:00", 1, 7);
+INSERT INTO officeVisit(patientMID, visitDate, locationID, apptTypeID, household_smoking_status, patient_smoking_status, notes) VALUES (1, "2017-02-22 00:00:00", 1, 7, 1, 3, 'This is a note');
 INSERT INTO obstetricsVisit(officeVisitID, weeksPregnant, fhr, multiplicity, lowLyingPlacentaObserved, imageType) VALUES (51, 32, 41, 1, false, "image.jpg");
